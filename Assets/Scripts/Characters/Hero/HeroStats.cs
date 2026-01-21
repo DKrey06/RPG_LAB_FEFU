1a8cfb35547114fe5afaaaa6a485e564d7d9362e
Dmitry <mrdmitry2006@mail.ru>
2026-01-21 12:06:34 +1000

    Preparation before writing a hero class

---
+++ Assets/Scripts/Characters/Hero/HeroStats.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+//Зона Димы A. Создать класс героя с нужными полями (HP, MP, Str, Dex, Def, Level, XP)
+public class HeroStats : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
